c5c3df4a5b57e6dc6e515a1aa7815238996ae800
Cogentx <code@cogentx.com>
2022-01-05 10:17:34 -0330

    Add Firebase Firestore DB to Angular App

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'cogentx-360ai-app',
+    appId: '1:99012026996:web:4d0047e2362289075f2721',
+    databaseURL: 'https://cogentx-360ai-app.firebaseio.com',
+    storageBucket: 'cogentx-360ai-app.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDV7KRqzWnZEsZPvhoT8Ww0Mwa2hOfer3c',
+    authDomain: 'cogentx-360ai-app.firebaseapp.com',
+    messagingSenderId: '99012026996',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'cogentx-360ai-app',
+    appId: '1:99012026996:web:4d0047e2362289075f2721',
+    databaseURL: 'https://cogentx-360ai-app.firebaseio.com',
+    storageBucket: 'cogentx-360ai-app.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDV7KRqzWnZEsZPvhoT8Ww0Mwa2hOfer3c',
+    authDomain: 'cogentx-360ai-app.firebaseapp.com',
+    messagingSenderId: '99012026996',
+  },
   production: false
 };
 
