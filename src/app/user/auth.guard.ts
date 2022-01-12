78b58fdadc88c96248c4e001b23596c4cfafcfaf
Cogentx <code@cogentx.com>
2022-01-12 14:08:18 -0330

    Protect routes with guards - add AuthGuard

---
+++ src/app/user/auth.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Auth } from '@angular/fire/auth';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
+import { SnackService } from '../services/snack.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(
+    private readonly auth: Auth, 
+    private snackBar: SnackService) {}
+
+  async canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Promise<boolean> {
+    
+    const user = await this.auth.currentUser;
+    const isLoggedin = !!user;
+
+    if (!isLoggedin) {
+      this.snackBar.authError();
+    }
+    
+    return isLoggedin;
+  }
+  
+}
